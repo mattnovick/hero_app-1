cb54cfd7d2398fd97622dd29bd5a05e1bd8e54fd
Kareem Grant <kareem@getuserwise.com>
2016-03-13 14:47:38 -0400

    Adds error handling

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -12,9 +12,15 @@ class CharactersController < ApplicationController
   end
 
   def create
-    @character = Character.create(character_params)
+    @character = Character.new(character_params)
 
-    redirect_to character_path(@character)
+    if @character.save
+      flash[:notice] = "Character was successfully created"
+      redirect_to character_path(@character)
+    else
+      flash.now[:error] = "An error occurred while saving this character"
+      render :new
+    end
   end
 
   def edit
@@ -23,16 +29,20 @@ class CharactersController < ApplicationController
 
   def update
     @character = Character.find(params[:id])
-    @character.update(character_params)
 
-    redirect_to character_path(@character)
+    if @character.update(character_params)
+      redirect_to character_path(@character), notice: "Character was successfully updated!"
+    else
+      flash.now[:error] = "An error occurred when updating this Character"
+      render :edit
+    end
   end
 
   def destroy
     @character = Character.find(params[:id])
     @character.destroy
 
-    redirect_to characters_path
+    redirect_to characters_path, notice: "Character was successfully deleted"
   end
 
   private
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,2 +1,4 @@
 class Character < ActiveRecord::Base
+  validates :hero_name, presence: true
+  validates :hero_name, uniqueness: { case_sensitive: false }
 end
